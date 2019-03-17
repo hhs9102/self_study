48c1935bf1ada0aad23540d00703979118dd2667
hsham <hhs9102@naver.com>
2019-03-17 13:31:35 +0900

    thymeleaf 추가

--- MockMvcProject/src/main/java/com/example/demo/MainController.java
+++ MockMvcProject/src/main/java/com/example/demo/MainController.java
@@ -1,13 +1,21 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@RestController
+@Controller
 public class MainController {
+	
 	@RequestMapping("/")
-	public String index() {
-		return "Main Page!";
+	public String root() {
+		return "index";   // html name
+	}	
+	@GetMapping("/demo")
+	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);		
+		return "demo";   // html name
 	}
-
 }
