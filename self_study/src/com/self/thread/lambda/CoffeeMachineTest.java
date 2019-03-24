ae21d7d5b8c0a11ff6882946617797351ac1ed84
hsham <hhs9102@naver.com>
2019-03-24 21:54:14 +0900

    lambda expression으로 Thread 생성

---
+++ self_study/src/com/self/thread/lambda/CoffeeMachineTest.java
@@ -0,0 +1,27 @@
+package com.self.thread.lambda;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class CoffeeMachineTest {
+
+	public static void main(String[] args) {
+
+		final AtomicInteger count = new AtomicInteger(1);
+		
+		CoffeeMachine coffeeMachine = new CoffeeMachine();
+		for(int i=0; i<10; i++) {
+			new Thread(() -> {
+				try {
+					int cnt = count.getAndAdd(1);
+					String employee = "직원"+cnt;
+					coffeeMachine.use(employee);
+					Thread.sleep(3000);
+					coffeeMachine.done(employee);
+				} catch (InterruptedException e) {
+					System.out.println(e.getMessage());
+				}
+			}).start();
+		}
+	}
+
+}
