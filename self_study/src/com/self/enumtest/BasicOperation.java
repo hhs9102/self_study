4dbccb91a31d2d28d3d8d9daef635ac288e3cd35
Hosik Ham <hhs9102@naver.com>
2019-04-21 14:54:16 +0900

    conflict

---
+++ self_study/src/com/self/enumtest/BasicOperation.java
@@ -0,0 +1,45 @@
+package com.self.enumtest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import lombok.Getter;
+
+@Getter
+public enum BasicOperation {
+
+	PLUS("+", "P"){
+		public double apply(double x, double y) {
+			return x+y;
+		}
+	}
+	,MINUS("-", "M"){
+		public double apply(double x, double y) {
+			return x-y;
+		}
+	}
+	;
+	
+	String symbol;
+	String code;
+	
+	public abstract double apply(double x, double y);
+	private static Map<String, BasicOperation> basicOperationMap = new HashMap<String, BasicOperation>();
+	
+	private BasicOperation(String symbol, String code) {
+		this.symbol = symbol;
+		this.code = code;
+	}
+	
+	public static BasicOperation getBasicOperationByCode(String code) {
+		if(basicOperationMap.isEmpty()) {
+			for(BasicOperation bo : BasicOperation.values()) {
+				basicOperationMap.put(bo.getCode(), bo);
+			}
+		}
+		if(basicOperationMap.containsKey(code)) {
+			return basicOperationMap.get(code);
+		}
+		throw new RuntimeException(code + " not exists");
+	}
+}
---
+++ self_study/src/com/self/generics/stack/Stack.java
@@ -0,0 +1,53 @@
+package com.self.generics.stack;
+
+import java.util.Arrays;
+
+import lombok.extern.java.Log;
+
+@Log
+public class Stack<E> {
+
+	private E[] element;
+	private int size = 0;
+	private static final int DEFAULT_SIZE = 10;
+	
+	@SuppressWarnings("unchecked")
+	public Stack(){
+		element = (E[])new Object[DEFAULT_SIZE];
+	}
+	
+	public void push(E e) {
+		ensureCapacity();
+		element[size++] = e;
+	}
+	
+	public E pop(){
+		if(isEmpty()) {
+			throw new RuntimeException("Stack is empty.");
+		}
+		E result = element[--size];
+		element[size] = null;	//메모리 초기화
+		return result;
+	}
+	
+	public void ensureCapacity() {
+		if(element.length == size) {
+			element = Arrays.copyOf(element, element.length + DEFAULT_SIZE);
+		}
+	}
+	
+	public boolean isEmpty() {
+		return size == 0;
+	}
+	
+	public static void main(String[] args) {
+		Stack<String> strStack = new Stack<>();	//7버전 이상부터 생략가능.
+		strStack.push("1");
+		strStack.push("2");
+		strStack.push("3");
+		
+		while(!strStack.isEmpty()) {
+			log.info(strStack.pop());
+		}
+	}
+}
