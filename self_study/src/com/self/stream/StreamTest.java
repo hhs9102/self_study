067b159e20060ff44616ba678fdf63cd9aa7c5e6
hamhosik <hhs9102@naver.com>
2019-10-06 23:34:26 +0900

    stream Test

--- self_study/src/com/self/stream/StreamTest.java
+++ self_study/src/com/self/stream/StreamTest.java
@@ -2,9 +2,15 @@ package com.self.stream;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.joining;
 
 public class StreamTest {
 
@@ -48,6 +54,44 @@ public class StreamTest {
                 .filter(i -> i>5)
                 .noneMatch(i -> i==20);
         System.out.println(isNotExists);
+    }
+
+    @Test
+    public void testStream(){
+        List<String> l = new ArrayList(Arrays.asList("one", "two"));
+        Stream<String> sl = l.stream();
+        l.add("three");
+        l.add("one");
+        l.add("ten");
+        String s = sl.collect(joining(" "));
+        System.out.println(s);
+
+        System.out.println(l.stream()
+        .reduce("", String::concat));
+        System.out.println(l.stream()
+        .reduce("", String::concat));
+
+
+        for(String element : l.stream().collect(Collectors.toList())){
+            System.out.println(element);
+        }
+
+        System.out.println(l.stream().map(str -> str.toUpperCase()).collect(Collectors.joining(",")));
+
+        System.out.println(l.stream().map(str -> str.substring(0,1)).collect(Collectors.groupingBy(str ->str, counting())));
+
+        String findAnyStr = l.stream()
+                .filter(str -> s.startsWith("e"))
+                .findAny()
+                .orElse("none");
+        System.out.println(findAnyStr);
+
+        String findAnyStr2 = l.stream()
+                .filter(str -> s.startsWith("o"))
+                .findAny()
+                .orElse("none");
+        System.out.println(findAnyStr2);
+
     }
 }
 
